06592f2190c75b7b0727fdc0738b4b25c80d02c2
VadimGouskov <vadimgouskov@hotmail.com>
2018-12-08 17:11:26 +0100

    Backend interface
    added:
     - endpoint to test the transitmap request ( w/ localhost backend )
    - generic requestFromBackend method to do a REST GET request to an endpoint (wo/ parameters for now)

--- SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/LinkController.java
+++ SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/LinkController.java
@@ -5,15 +5,14 @@ import be.uantwerpen.sc.services.LinkControlService;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -27,7 +26,9 @@ public class LinkController {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    @Value("${bot.ip}")
+    // TODO get the values from application.properties
+//    @Value("${bot.ip}")
+    @Value("localhost")
     String ip;
 
     @Value("${drone.port}")
@@ -38,6 +39,10 @@ public class LinkController {
 
     @Value("${robot.port}")
     String robotPort;
+
+
+
+
 
 //    @Value("classpath:json/resource-data.txt")
 //    Resource resourceFile;
@@ -62,8 +67,9 @@ public class LinkController {
     @RequestMapping(value = "pathlinks", method = RequestMethod.GET)
     public JSONArray getPathLinks(@RequestParam int pidstart, @RequestParam int pidend ) {
         JSONArray linkArray = new JSONArray();
+        // activate when testing without database (reads data from jsonfiles)
         boolean TEST = true;
-
+        // header for testing purposes
         JSONObject header = new JSONObject();
         header.put("pidstart", pidstart);
         header.put("pidend", pidend);
@@ -99,30 +105,65 @@ public class LinkController {
             }
         }else{
             // TODO get linkArrays from the backends
-                // TODO build call
+                // TODO build calls and parsing
 
         }
         return linkArray ;
 
     }
 
-    /* TEST METHOD: getDroneLinks calling this endpooint triggers a call to get the drone links
+    /* TEST METHOD: getDroneLinks calling this endpooint triggers a call to get a backennds transitlinks
      *  endpoint to get the entire transitmap, MaaS will call this
      *  @return an array of links of the drone transit map
      */
-    @RequestMapping(value = "droneLinks", method = RequestMethod.GET)
-    public JSONArray getPathLinks( ) {
+    @RequestMapping(value = "testLinks", method = RequestMethod.GET)
+    public JSONArray gettestLinks( ) {
+        return requestFromBackend("http://localhost", "8081","/link/transitmap" );
+    }
+
+    /* a generic method do do a request to a server and parse the response to a JSONArray
+     *
+     * @param int :
+     *
+     * @return JSONArray : backend response
+     */
+    private JSONArray requestFromBackend(String ip, String port, String endpoint){
+        String responseLine;
+        String response = "";
+
+        // used to parse strings to json
+        JSONParser parser = new JSONParser();
         JSONArray linkArray = new JSONArray();
         try {
-            URL urlDrone = new URL("http://" + ip + ":" + dronePort + "/posAll");
-            HttpURLConnection connDrone = (HttpURLConnection) urlDrone.openConnection();
-            // TODO
+            URL urlCar = new URL(ip + ":" + port + endpoint);
+            HttpURLConnection conn = (HttpURLConnection) urlCar.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                while ((responseLine = br.readLine()) != null) {
+                    response += responseLine;
+                }
+                System.out.println("got: " + response + "\nfrom:" + ip + ":" + port + endpoint);
+                try {
+                    JSONArray jsonArray = (JSONArray) parser.parse(response);
+                    return jsonArray;
+                }
+                catch(ParseException e){
+                    System.out.println("could not parse folowwing string: " + response);
+                    System.out.println(e.getStackTrace());
+                }
+            } else {
+                System.out.println("Request Failed, Responsecode returned: " + conn.getResponseCode());
+            }
+
         }catch(IOException e){
             e.printStackTrace();
         }
 
         return linkArray ;
-
     }
 }
 
