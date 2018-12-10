8031a8a970b1d804c9d8b017e61e85ce1bf82e36
Yunus Emre <emrepsyigit@gmail.com>
2018-12-10 16:31:06 +0100

    Changed the jobListService to the latest one from MaaS and added the JobService and JobRepository - Emre

--- SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/JobDispatchController.java
+++ SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/JobDispatchController.java
@@ -5,13 +5,13 @@ import be.uantwerpen.sc.services.JobListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/job/")
 public class JobDispatchController {
+
     @Autowired
     private JobListService jobListService;
 
@@ -21,22 +21,23 @@ public class JobDispatchController {
     private static final Logger logger = LoggerFactory.getLogger(JobDispatchController.class);
 
     /**
-     * @param jobList The jobList object to save
+     * @param jobList The jobList object to save (received from MaaS)
      */
     @RequestMapping(value = "saveorder", method = RequestMethod.POST)
     public void saveOrder(@RequestBody JobList jobList) {
         logger.info("Test: Saving jobList");
         System.out.println(jobList.toString());
+        System.out.println("Vehicle type in controller: " + jobList.getJobs().get(0).getTypeVehicle());
         //jobListService.saveOrder(jobList);
     }
 
     /**
-     * Dispatching
+     * Dispatching (This request comes from MaaS
      */
     @RequestMapping(value = "dispatch", method = RequestMethod.POST)
     public void dispatch() {
         logger.info("Test Dispatching");
-        //jobListService.dispatch2Core();
+        //jobListService.dispatchToCore();
     }
 
 //    // Define REST bean
@@ -44,5 +45,4 @@ public class JobDispatchController {
 //    public RestTemplate restTemplate() {
 //        return new RestTemplate();
 //    }
-
 }
---
+++ SmartCity Core/src/main/java/be/uantwerpen/sc/repositories/JobRepository.java
@@ -0,0 +1,15 @@
+package be.uantwerpen.sc.repositories;
+
+import be.uantwerpen.sc.model.Job;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * NV 2018
+ */
+@Repository
+public interface JobRepository extends CrudRepository<Job, Long> {
+    List<Job> findAll();
+}
