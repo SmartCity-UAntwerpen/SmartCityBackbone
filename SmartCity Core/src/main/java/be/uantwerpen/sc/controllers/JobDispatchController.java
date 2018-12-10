3ed4a02650f24f7666cdff34a61284692d91c2ff
Imre Liessens <imre.liessens@telenet.be>
2018-12-10 17:33:49 +0100

    Moved completeJob van MaaS

--- SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/JobDispatchController.java
+++ SmartCity Core/src/main/java/be/uantwerpen/sc/controllers/JobDispatchController.java
@@ -2,28 +2,29 @@ package be.uantwerpen.sc.controllers;
 
 import be.uantwerpen.sc.model.JobList;
 import be.uantwerpen.sc.services.JobListService;
+import be.uantwerpen.sc.services.JobService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/job/")
 public class JobDispatchController {
 
     @Autowired
     private JobListService jobListService;
 
-//    @Autowired
-//    private RestTemplate restTemplate;
+    @Autowired
+    private JobService jobService;
 
     private static final Logger logger = LoggerFactory.getLogger(JobDispatchController.class);
 
     /**
      * @param jobList The jobList object to save (received from MaaS)
      */
-    @RequestMapping(value = "saveorder", method = RequestMethod.POST)
+    @RequestMapping(value = "/job/saveorder", method = RequestMethod.POST)
     public void saveOrder(@RequestBody JobList jobList) {
         logger.info("Test: Saving jobList");
         System.out.println(jobList.toString());
@@ -34,15 +35,37 @@ public class JobDispatchController {
     /**
      * Dispatching (This request comes from MaaS
      */
-    @RequestMapping(value = "dispatch", method = RequestMethod.POST)
+    @RequestMapping(value = "/job/dispatch", method = RequestMethod.POST)
     public void dispatch() {
         logger.info("Test Dispatching");
         //jobListService.dispatchToCore();
     }
 
-//    // Define REST bean
-//    @Bean
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
+    @RequestMapping(value = "/completeJob/{idJob}", method = RequestMethod.GET)
+    @ResponseBody
+    public String completeJob(@PathVariable Long idJob) {
+        logger.info("Job " + idJob + " is complete");
+        for (JobList jl : jobListService.findAll()) {
+            if (jl.getJobs().get(0).getId().equals(idJob)) {
+                jl.getJobs().remove(0);
+                jobService.delete(idJob);
+                logger.info("Rendezvous job with id " + idJob + " is deleted because it was complete");
+            } else if (jl.getJobs().size() > 1 && jl.getJobs().get(1).getId().equals(idJob)) {
+                // rendezvous
+                jl.getJobs().remove(1);
+                jobService.delete(idJob);
+                logger.info("Job " + idJob + " is deleted because it was complete");
+            }
+
+            if (jl.getJobs().isEmpty()) {
+                jobListService.deleteOrder(jl.getId());
+                logger.info("Delete order");
+            } else {
+                jobListService.dispatchToCore();
+                logger.info("Dispatch next job");
+            }
+        }
+
+        return "ok";
+    }
 }
